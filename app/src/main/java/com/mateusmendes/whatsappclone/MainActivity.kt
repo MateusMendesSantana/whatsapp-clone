c4d813b9f618441813b5986daa37ffa9312d2f59
Mateus Mendes <mateusmendessantana@hotmail.com>
2019-06-30 19:52:22 -0300

    Initial layout

--- app/src/main/java/com/mateusmendes/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/mateusmendes/whatsappclone/MainActivity.kt
@@ -2,11 +2,25 @@ package com.mateusmendes.whatsappclone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.elevation = 0f
+        tab_layout.setTabWidthAsWrapContent(0)
+    }
+
+    private fun TabLayout.setTabWidthAsWrapContent(tabPosition: Int) {
+        val layout = (this.getChildAt(0) as LinearLayout).getChildAt(tabPosition) as LinearLayout
+        val layoutParams = layout.layoutParams as LinearLayout.LayoutParams
+
+        layoutParams.weight = 0f
+        layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT
+        layout.layoutParams = layoutParams
     }
 }
