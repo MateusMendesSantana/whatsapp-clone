13f89c510e83187a5ff94b967b30a9abcdb2c839
Mateus Mendes <mateusmendessantana@hotmail.com>
2019-07-04 20:13:24 -0300

    Add floating button in chatsFragment

---
+++ app/src/main/java/com/mateusmendes/whatsappclone/view/ChatActivity.kt
@@ -0,0 +1,13 @@
+package com.mateusmendes.whatsappclone.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mateusmendes.whatsappclone.R
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
